2b1fe30e7daf80fd3aef68ae83f7c86c190fb54c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-17 22:58:08 +0900

    画面遷移なしでメッセージ送信

--- sub.php
+++ sub.php
@@ -1,40 +1,22 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php
+require './vendor/autoload.php';
+Dotenv\Dotenv::createImmutable(__DIR__)->load();
+$host = $_ENV['HOST'];
+$DBname = $_ENV['DBNAME'];
+$user = $_ENV['USER'];
+$passwd = $_ENV['PASSWD'];
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>掲示板</title>
-</head>
+if (!empty($_POST["name"]) && !empty($_POST["message"])) {
+    $name = htmlspecialchars($_POST["name"], ENT_QUOTES);
+    $message = htmlspecialchars($_POST["message"], ENT_QUOTES);
 
-<body>
+    $db = new PDO("mysql:host=$host;dbname=$DBname", $user, $passwd);
 
-    <?php
-    require './vendor/autoload.php';
-    Dotenv\Dotenv::createImmutable(__DIR__)->load();
-    $host = $_ENV['HOST'];
-    $DBname = $_ENV['DBNAME'];
-    $user = $_ENV['USER'];
-    $passwd = $_ENV['PASSWD'];
+    $db->query("INSERT INTO tb1(no, name, message, time)
+            VALUES(NULL, '$name', '$message', NOW())");
 
-    if (!empty($_POST["name"]) && !empty($_POST["message"])) {
-        $name = htmlspecialchars($_POST["name"], ENT_QUOTES);
-        $message = htmlspecialchars($_POST["message"], ENT_QUOTES);
+} else {
+}
 
-        $db = new PDO("mysql:host=$host;dbname=$DBname", $user, $passwd);
-
-        $db->query("INSERT INTO tb1(no, name, message, time)
-             VALUES(NULL, '$name', '$message', NOW())");
-
-        print "<h1>コメント送信完了しました</h1>";
-        print "<p><a href=index.html>掲示板へ戻る</a></p>";
-    } else {
-        print "<h1>※フォームが入力されていません</h1>";
-        print "<p><a href=index.html>掲示板へ戻る</a></p>";
-    }
-
-    ?>
-
-</body>
-
-</html>
+exit;
+?>
