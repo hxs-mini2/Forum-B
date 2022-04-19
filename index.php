ac1a1f1f4c500ae16dff15069f21dc7d944dfd49
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-19 23:24:06 +0900

    動的に掲示板へのリンク作成

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="utf-8">
+	<title>掲示板ハブ</title>
+</head>
+
+<body>
+
+	<?php
+		require './keiziban/vendor/autoload.php';
+		Dotenv\Dotenv::createImmutable(__DIR__)->load();
+		$host = $_ENV['HOST'];
+		$DBname = $_ENV['DBNAME'];
+		$user = $_ENV['USER'];
+		$passwd = $_ENV['PASSWD'];
+		
+		$db = new PDO("mysql:host=$host;dbname=$DBname", "$user", "$passwd");
+    	$n = $db->query("SHOW TABLES");
+		while ($i = $n->fetch()) {
+			$data[] = $i[0];
+		}
+
+		echo "<table class='Forums'>";
+		echo "<tr><th>NO.</th><td>掲示板一覧</td></tr>";
+		$count = 1;
+		foreach ($data as $value) {
+			echo "<tr><th>$count</th><td><a href=\"keiziban/index.php?table%5B%5D='$value'\">$value</a></td></tr>";
+			$count++;
+		}
+		echo "</table>";
+	?>
+
+</body>
+
+</html>
