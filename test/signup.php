b4c2ea6643c27ac9db8795447d8600b7e9380359
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-21 17:09:48 +0900

    DBへユーザ名・メールアドレス・パスワード（ハッシュ値）を保存

--- test/signup.php
+++ test/signup.php
@@ -1,5 +1,5 @@
 <h1>新規会員登録</h1>
-<form action="register.php" method="post">//処理を行う宛先を指定
+<form action="register.php" method="post">
 <div>
     <label>名前：<label>
     <input type="text" name="name" required>
