7d5147ced2fb9313e16fad5d0ce613594dd614b9
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-23 17:00:54 +0900

    OTPが一致した場合のみDBへと保存する

--- test/mail.php
+++ test/mail.php
@@ -1,12 +1,29 @@
 <?php
 session_start();
+require dirname(__FILE__).'/../vendor/autoload.php';
+Dotenv\Dotenv::createImmutable(__DIR__.'/..')->load();
+$host = $_ENV['HOST'];
+$DBname = $_ENV['DBACCOUNT'];
+$user = $_ENV['USER'];
+$passwd = $_ENV['PASSWD'];
+$name = $_SESSION['name'];
+$mail = $_SESSION['mail'];
+$pass = $_SESSION['pass'];
 $rand = $_SESSION['code'];
 $otp = $_POST['code'];
 
 if ($rand == $otp) {
-	unset($_SESSION['code']);
+	$_SESSION = array();
+	$_POST = array();
 
-	echo "登録しました．<br>";
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$DBname", $user, $passwd);
+        $db->query("INSERT INTO user(no, name, mail, pass) VALUES(NULL, '$name', '$mail', '$pass')");
+        echo "<br>登録できました．<br>";		
+    } catch (Exception $e) {
+        echo "<br>登録できませんでした．<br>";
+    }
+
 } else {
 	echo "コードが間違っています<br><br>";
     echo "<h1>メール承認（再）</h1>";
