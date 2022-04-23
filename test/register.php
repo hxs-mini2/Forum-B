5ee4963c8c7cf2e2cbdccb14f0858bc9a4b7098c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-23 15:52:31 +0900

    ランダムな数字を入力されたメール宛てに送信

--- test/register.php
+++ test/register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require dirname(__FILE__).'/../vendor/autoload.php';
 Dotenv\Dotenv::createImmutable(__DIR__.'/..')->load();
 $name = $_POST['name'];
@@ -16,14 +17,23 @@ $i = $n->fetch();
 if (!empty($i['mail']) || !empty($i['name'])) {
     echo "同じメールアドレスまたは，同じユーザIDが存在します。<br>";
 } else {
-    try {
-        $db = new PDO("mysql:host=$host;dbname=$DBname", $user, $passwd);
-        $db->query("INSERT INTO user(no, name, mail, pass) VALUES(NULL, '$name', '$mail', '$pass')");
-        echo "<br>登録できました．<br>";
-    } catch (Exception $e) {
-        echo "<br>登録できませんでした．<br>";
+    $rand = mt_rand(100000, 999999);
+
+    mb_language("Japanese");
+    mb_internal_encoding("UTF-8");
+
+    $to = $mail;
+    $title = 'HxS掲示板・新規登録';
+    $message = 'コード : '.$rand;
+    $headers = "From: hoge21119@gmail.com";
+
+    if (mb_send_mail($to, $title, $message, $headers)) {
+        echo "メール送信成功です";
+    } else {
+        echo "メール送信失敗です";
     }
 }
 
+echo "<br><br>";
 echo "<a href='signup.php'>戻る</a>";
 ?>
