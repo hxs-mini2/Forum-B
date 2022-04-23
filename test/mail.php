68ceecd94e9feb667a5a799fdb5d3a3acf08bd98
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-23 16:45:46 +0900

    OTPの検証実装

--- test/mail.php
+++ test/mail.php
@@ -1,8 +1,20 @@
 <?php
 session_start();
 $rand = $_SESSION['code'];
-unset($_SESSION['code']);
+$otp = $_POST['code'];
 
-echo $rand;
+if ($rand == $otp) {
+	unset($_SESSION['code']);
+
+	echo "登録しました．<br>";
+} else {
+	echo "コードが間違っています<br><br>";
+    echo "<h1>メール承認（再）</h1>";
+    echo "<form action='mail.php' method='post'>";
+    echo "<label>コード</label><br>";
+    echo "<input type='text' name='code' required>";
+    echo "<input type='submit' value='確認'>";
+    echo "</form>";
+}
 
 ?>
